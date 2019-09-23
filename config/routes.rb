44085b4303287855847c6af3183135cc888afd42
Sarah Tatro <statrox4@gmail.com>
2019-09-23 13:05:52 -0600

    Remove root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'root'
-  
+
   get '/:id' => 'short_links#show'
   resources :short_links, only: [:show]
 
