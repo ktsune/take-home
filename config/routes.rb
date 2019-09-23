c47f6105da5f5ba9ccfef19c3178bbdfd0514025
Sarah Tatro <statrox4@gmail.com>
2019-09-23 12:29:19 -0600

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'root'
+  
   get '/:id' => 'short_links#show'
   resources :short_links, only: [:show]
 
